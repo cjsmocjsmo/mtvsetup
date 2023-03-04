0de6901e1524edc599f67803cb76bdce0283e9b1
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-03-04 15:29:08 -0800

    added config file

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ mod mtv_process_mp3s;
 fn main() {
     let dockervar = mtv_env_vars::get_docker_var();
     if dockervar == "docker var not set".to_string() {
-        mtv_env_vars::set_all_env_vars();
+        mtv_env_vars::read_config();
         println!(
             "should be /media/charliepi/FOO/media :\n {}",
             env::var("MTV_MEDIA_PATH").unwrap()
@@ -22,18 +22,20 @@ fn main() {
         println!("docker var is set so docker will set the env vars for us");
     };
 
+    mtv_env_vars::read_config();
+
     mtv_clean::clean_meta();
 
-    mtv_process_music_images::process_music_images();
+    // mtv_process_music_images::process_music_images();
 
     mtv_process_mp3s::process_mp3s();
 
 
-    let _movievec = mtv_walk_dirs::walk_movies_dir();
-    let _moviethumbvec = mtv_walk_dirs::walk_movies_thumb_dir();
+    // let _movievec = mtv_walk_dirs::walk_movies_dir();
+    // let _moviethumbvec = mtv_walk_dirs::walk_movies_thumb_dir();
 
-    let _music_metadata = mtv_walk_dirs::walk_metadata_music();
-    let _movies_metadata = mtv_walk_dirs::walk_metadata_movies();
+    // let _music_metadata = mtv_walk_dirs::walk_metadata_music();
+    // let _movies_metadata = mtv_walk_dirs::walk_metadata_movies();
 
     let mtv_media_path = env::var("MTV_MEDIA_PATH").expect("$MTV_MEDIA_PATH is not set");
 
--- src/mtv_env_vars.rs
+++ src/mtv_env_vars.rs
@@ -1,4 +1,8 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+// use yaml_rust::yaml;
+use yaml_rust::YamlLoader;
 
 pub fn get_docker_var() -> String {
     let docker_var_results = env::var("MTV_DOCKER_VAR");
@@ -22,32 +26,47 @@ pub fn set_env_var(p1: String, p2: String) -> Result<(), Box<dyn std::error::Err
     Ok(())
 }
 
-pub fn set_all_env_vars() {
-    let media1: String = String::from("MTV_MEDIA_PATH");
-    let media2 = String::from("/media/charliepi/FOO/media");
-    let _media_env_set = set_env_var(media1, media2).is_ok();
+pub fn read_config() {
+    let mut file = File::open("./src/config.yaml").expect("Unable to open file");
+    let mut contents = String::new();
 
-    let music1 = String::from("MTV_MUSIC_PATH");
-    let music2 = String::from("/media/charliepi/FOO/media/music/C/");
-    let _music_env_set = set_env_var(music1, music2).is_ok();
+    file.read_to_string(&mut contents)
+        .expect("Unable to read file");
 
-    let music_thumb1 = String::from("MTV_MUSIC_THUMBNAIL_PATH");
-    let music_thumb2 = String::from("/media/charliepi/FOO/media/music_thumbnails");
-    let _music_env_set = set_env_var(music_thumb1, music_thumb2).is_ok();
+    let docs = YamlLoader::load_from_str(&contents).unwrap();
+    for d in docs {
+        let music0 = "MTV_MEDIA_PATH".to_string();
+        let music1 = d["MTV_MEDIA_PATH"].as_str().unwrap().to_string();
+        set_env_var(music0, music1).unwrap();
 
-    let movies1 = String::from("MTV_MOVIES_PATH");
-    let movies2 = String::from("/media/charliepi/FOO/media/movies");
-    let _music_env_set = set_env_var(movies1, movies2).is_ok();
+        let music2 = "MTV_MUSIC_PATH".to_string();
+        let music3 = d["MTV_MUSIC_PATH"].as_str().unwrap().to_string();
+        set_env_var(music2, music3).unwrap();
 
-    let movies_thumb1 = String::from("MTV_MOVIES_THUMBNAIL_PATH");
-    let movies_thumb2 = String::from("/media/charliepi/FOO/media/movies_thumbnails");
-    let _movies_env_set = set_env_var(movies_thumb1, movies_thumb2).is_ok();
+        let music4 = "MTV_MUSIC_THUMBNAIL_PATH".to_string();
+        let music5 = d["MTV_MUSIC_THUMBNAIL_PATH"].as_str().unwrap().to_string();
+        set_env_var(music4, music5).unwrap();
 
-    let movies_metadata1 = String::from("MTV_MOVIES_METADATA_PATH");
-    let movies_metadata2 = String::from("/media/charliepi/FOO/media/metadata_movies");
-    let _movies_metadata_env_set = set_env_var(movies_metadata1, movies_metadata2).is_ok();
+        let music6 = "MTV_MUSIC_METADATA_PATH".to_string();
+        let music7 = d["MTV_MUSIC_METADATA_PATH"].as_str().unwrap().to_string();
+        set_env_var(music6, music7).unwrap();
 
-    let music_metadata1 = String::from("MTV_MUSIC_METADATA_PATH");
-    let music_metadata2 = String::from("/media/charliepi/FOO/media/metadata_music");
-    let _music_metadata_env_set = set_env_var(music_metadata1, music_metadata2).is_ok();
+        let music8 = "MTV_MOVIES_PATH".to_string();
+        let music9 = d["MTV_MOVIES_PATH"].as_str().unwrap().to_string();
+        set_env_var(music8, music9).unwrap();
+
+        let music10 = "MTV_MOVIES_THUMBNAIL_PATH".to_string();
+        let music11 = d["MTV_MOVIES_THUMBNAIL_PATH"].as_str().unwrap().to_string();
+        set_env_var(music10, music11).unwrap();
+
+        let music12 = "MTV_MOVIES_METADATA_PATH".to_string();
+        let music13 = d["MTV_MOVIES_METADATA_PATH"].as_str().unwrap().to_string();
+        set_env_var(music12, music13).unwrap();
+
+        let offset1 = String::from("MTV_OFFSET");
+        let offset2 = String::from("25");
+        set_env_var(offset1, offset2).unwrap();
+
+
+    }
 }
