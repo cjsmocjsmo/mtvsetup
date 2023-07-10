f446483873e834d687975860697dc91d83dc1c11
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-07-10 11:48:49 -0700

    added clean.sh

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remove all files created by the build process.
+sudo rm -rf //home/pi/mtvsetup/thumbnails;
+mkdir /home/pi/mtvsetup/thumbnails;
+
+rm -rf /home/pi/mtvsetup/mtv.db;
+touch /home/pi/mtvsetup/mtv.db;
+
+git pull;
