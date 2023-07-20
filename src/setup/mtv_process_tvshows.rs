3e5e92556da2e3132c8c19cf0a7ab0047448525b
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-07-20 09:14:18 -0700

    update

--- src/setup/mtv_process_tvshows.rs
+++ src/setup/mtv_process_tvshows.rs
@@ -3,133 +3,133 @@ use crate::setup::mtv_types;
 use rusqlite::Connection;
 use regex::Regex;
 
-fn get_tv_catagory(x: &String) -> String {
-    let mut baz: String = String::new();
-    if x.contains("S1") {
-        let x_split = x.split("S1");
-        let mut x_split_vec = vec![];
-        for xs in x_split {
-            x_split_vec.push(xs);
-        }
-        let string_to_split = x_split_vec[1].to_string();
-        let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
-        println!("foo: {}", foo);
-        let mut foo_split_vec = Vec::new();
-        let foo_split = foo.split("/");
-        for f in foo_split {
-            foo_split_vec.push(f);
-        }
+// fn get_tv_catagory(x: &String) -> String {
+//     let mut baz: String = String::new();
+//     if x.contains("S1") {
+//         let x_split = x.split("S1");
+//         let mut x_split_vec = vec![];
+//         for xs in x_split {
+//             x_split_vec.push(xs);
+//         }
+//         let string_to_split = x_split_vec[1].to_string();
+//         let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
+//         println!("foo: {}", foo);
+//         let mut foo_split_vec = Vec::new();
+//         let foo_split = foo.split("/");
+//         for f in foo_split {
+//             foo_split_vec.push(f);
+//         }
 
-        baz = foo_split_vec[1].to_string();
-        println!("baz: {}", baz);
-    } else if x.contains("S2") {
-        let x_split = x.split("S2");
-        let mut x_split_vec = vec![];
-        for xs in x_split {
-            x_split_vec.push(xs);
-        }
-        let string_to_split = x_split_vec[1].to_string();
-        let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
-        println!("foo: {}", foo);
-        let mut foo_split_vec = Vec::new();
-        let foo_split = foo.split("/");
-        for f in foo_split {
-            foo_split_vec.push(f);
-        }
+//         baz = foo_split_vec[1].to_string();
+//         println!("baz: {}", baz);
+//     } else if x.contains("S2") {
+//         let x_split = x.split("S2");
+//         let mut x_split_vec = vec![];
+//         for xs in x_split {
+//             x_split_vec.push(xs);
+//         }
+//         let string_to_split = x_split_vec[1].to_string();
+//         let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
+//         println!("foo: {}", foo);
+//         let mut foo_split_vec = Vec::new();
+//         let foo_split = foo.split("/");
+//         for f in foo_split {
+//             foo_split_vec.push(f);
+//         }
 
-        baz = foo_split_vec[1].trim().to_string();
-        println!("baz: {}", baz);
-    } else if x.contains("S3") {
-        let x_split = x.split("S3");
-        let mut x_split_vec = vec![];
-        for xs in x_split {
-            x_split_vec.push(xs);
-        }
-        let string_to_split = x_split_vec[1].to_string();
-        let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
-        println!("foo: {}", foo);
-        let mut foo_split_vec = Vec::new();
-        let foo_split = foo.split("/");
-        for f in foo_split {
-            foo_split_vec.push(f);
-        }
+//         baz = foo_split_vec[1].trim().to_string();
+//         println!("baz: {}", baz);
+//     } else if x.contains("S3") {
+//         let x_split = x.split("S3");
+//         let mut x_split_vec = vec![];
+//         for xs in x_split {
+//             x_split_vec.push(xs);
+//         }
+//         let string_to_split = x_split_vec[1].to_string();
+//         let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
+//         println!("foo: {}", foo);
+//         let mut foo_split_vec = Vec::new();
+//         let foo_split = foo.split("/");
+//         for f in foo_split {
+//             foo_split_vec.push(f);
+//         }
 
-        baz = foo_split_vec[1].trim().to_string();
-        println!("baz: {}", baz);
-    } else if x.contains("S4") {
-        let x_split = x.split("S4");
-        let mut x_split_vec = vec![];
-        for xs in x_split {
-            x_split_vec.push(xs);
-        }
-        let string_to_split = x_split_vec[1].to_string();
-        let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
-        println!("foo: {}", foo);
-        let mut foo_split_vec = Vec::new();
-        let foo_split = foo.split("/");
-        for f in foo_split {
-            foo_split_vec.push(f);
-        }
+//         baz = foo_split_vec[1].trim().to_string();
+//         println!("baz: {}", baz);
+//     } else if x.contains("S4") {
+//         let x_split = x.split("S4");
+//         let mut x_split_vec = vec![];
+//         for xs in x_split {
+//             x_split_vec.push(xs);
+//         }
+//         let string_to_split = x_split_vec[1].to_string();
+//         let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
+//         println!("foo: {}", foo);
+//         let mut foo_split_vec = Vec::new();
+//         let foo_split = foo.split("/");
+//         for f in foo_split {
+//             foo_split_vec.push(f);
+//         }
 
-        baz = foo_split_vec[1].trim().to_string();
-        println!("baz: {}", baz);
-    } else if x.contains("S5") {
-        let x_split = x.split("S5");
-        let mut x_split_vec = vec![];
-        for xs in x_split {
-            x_split_vec.push(xs);
-        }
-        let string_to_split = x_split_vec[1].to_string();
-        let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
-        println!("foo: {}", foo);
-        let mut foo_split_vec = Vec::new();
-        let foo_split = foo.split("/");
-        for f in foo_split {
-            foo_split_vec.push(f);
-        }
+//         baz = foo_split_vec[1].trim().to_string();
+//         println!("baz: {}", baz);
+//     } else if x.contains("S5") {
+//         let x_split = x.split("S5");
+//         let mut x_split_vec = vec![];
+//         for xs in x_split {
+//             x_split_vec.push(xs);
+//         }
+//         let string_to_split = x_split_vec[1].to_string();
+//         let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
+//         println!("foo: {}", foo);
+//         let mut foo_split_vec = Vec::new();
+//         let foo_split = foo.split("/");
+//         for f in foo_split {
+//             foo_split_vec.push(f);
+//         }
 
-        baz = foo_split_vec[1].trim().to_string();
-        println!("baz: {}", baz);
-    } else if x.contains("S6") {
-        let x_split = x.split("S6");
-        let mut x_split_vec = vec![];
-        for xs in x_split {
-            x_split_vec.push(xs);
-        }
-        let string_to_split = x_split_vec[1].to_string();
-        let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
-        println!("foo: {}", foo);
-        let mut foo_split_vec = Vec::new();
-        let foo_split = foo.split("/");
-        for f in foo_split {
-            foo_split_vec.push(f);
-        }
+//         baz = foo_split_vec[1].trim().to_string();
+//         println!("baz: {}", baz);
+//     } else if x.contains("S6") {
+//         let x_split = x.split("S6");
+//         let mut x_split_vec = vec![];
+//         for xs in x_split {
+//             x_split_vec.push(xs);
+//         }
+//         let string_to_split = x_split_vec[1].to_string();
+//         let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
+//         println!("foo: {}", foo);
+//         let mut foo_split_vec = Vec::new();
+//         let foo_split = foo.split("/");
+//         for f in foo_split {
+//             foo_split_vec.push(f);
+//         }
 
-        baz = foo_split_vec[1].trim().to_string();
-        println!("baz: {}", baz);
-    } else if x.contains("S7") {
-        let x_split = x.split("S7");
-        let mut x_split_vec = vec![];
-        for xs in x_split {
-            x_split_vec.push(xs);
-        }
-        let string_to_split = x_split_vec[1].to_string();
-        let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
-        println!("foo: {}", foo);
-        let mut foo_split_vec = Vec::new();
-        let foo_split = foo.split("/");
-        for f in foo_split {
-            foo_split_vec.push(f);
-        }
+//         baz = foo_split_vec[1].trim().to_string();
+//         println!("baz: {}", baz);
+//     } else if x.contains("S7") {
+//         let x_split = x.split("S7");
+//         let mut x_split_vec = vec![];
+//         for xs in x_split {
+//             x_split_vec.push(xs);
+//         }
+//         let string_to_split = x_split_vec[1].to_string();
+//         let foo = Regex::new(r"S\d{2}").unwrap().split(&string_to_split).collect::<Vec<&str>>()[0].to_string();
+//         println!("foo: {}", foo);
+//         let mut foo_split_vec = Vec::new();
+//         let foo_split = foo.split("/");
+//         for f in foo_split {
+//             foo_split_vec.push(f);
+//         }
 
-        baz = foo_split_vec[1].to_string();
-    } else {
-        println!("Fuck")
-    }
+//         baz = foo_split_vec[1].to_string();
+//     } else {
+//         println!("Fuck")
+//     }
 
     
-    baz.trim().to_string()
-}
+//     baz.trim().to_string()
+// }
 
 fn get_season(astring: &String) -> String { 
     let my_captures: Vec<&str> = 
@@ -167,108 +167,109 @@ fn get_episode(astring: &String) -> String {
     epivec[1].to_string()
 }
 
-fn get_tv_name(x: &String) -> String {
+fn get_tv_catagory_name(x: &String) -> (String, String) {
     if x.contains("AlteredCarbon") {
-        return String::from("Altered Carbon");
+        return (String::from("AlteredCarbon"), String::from("Altered Carbon"));
     } else if x.contains("CowboyBebop") {
-        return String::from("Cowboy Bebop");
+        return (String::from("CowboyBebop"), String::from("Cowboy Bebop"));
     } else if x.contains("ForAllManKind") {
-        return String::from("For All ManKind");
+        return (String::from("ForAllManKind"), String::from("For All ManKind"));
     } else if x.contains("Foundation") {
-        return String::from("Foundation");
+        return (String::from("Foundation"), String::from("Foundation"));
     } else if x.contains("FuuBar") {
-        return String::from("FuuBar");
+        return (String::from("FuuBar"), String::from("FuuBar"));
     } else if x.contains("Halo") {
-        return String::from("Halo");
+        return (String::from("Halo"), String::from("Halo"));
     } else if x.contains("HFord1923") {
-        return String::from("1923");
+        return (String::from("HFord1923"), String::from("Harrison Ford 1923"));
     } else if x.contains("HouseOfTheDragon") {
-        return String::from("House Of The Dragon");
+        return (String::from("HouseOfTheDragon"), String::from("House Of The Dragon"));
     } else if x.contains("LostInSpace") {
-        return String::from("Lost In Space");
+        return (String::from("LostInSpace"), String::from("Lost In Space"));
     } else if x.contains("MastersOfTheUniverse") {
-        return String::from("Masters Of The Universe");
+        return (String::from("MastersOfTheUniverse"), String::from("Masters Of The Universe"));
     } else if x.contains("NightSky") {
-        return String::from("Night Sky");
+        return (String::from("NightSky"), String::from("Night Sky"));
     } else if x.contains("Orville") {
-        return String::from("Orville");
+        return (String::from("Orville"), String::from("Orville"));
     } else if x.contains("RaisedByWolves") {
-        return String::from("Raised By Wolves");
+        return (String::from("RaisedByWolves"), String::from("Raised By Wolves"));
     } else if x.contains("Silo") {
-        return String::from("Silo");
+        return (String::from("Silo"), String::from("Silo"));
     } else if x.contains("StarTrek/Discovery") {
-        return String::from("Discovery");
+        return (String::from("Discovery"), String::from("Discovery"));
     } else if x.contains("StarTrek/Picard") {
-        return String::from("Picard");
+        return (String::from("Picard"), String::from("Picard"));
     } else if x.contains("StarTrek/StrangeNewWorlds") {
-        return String::from("Strange New Worlds");
+        return (String::from("StrangeNewWorlds"), String::from("Strange New Worlds"));
     } else if x.contains("StarTrek/LowerDecks") {
-        return String::from("Lower Decks");
+        return (String::from("LowerDecks"), String::from("Lower Decks"));
     } else if x.contains("StarTrek/Prodigy") {
-        return String::from("Prodigy");
+        return (String::from("Prodigy"), String::from("Prodigy"));
     } else if x.contains("StarTrek/TNG") {
-        return String::from("The Next Generation");
+        return (String::from("TNG"), String::from("TNG"));
     } else if x.contains("StarTrek/STTV") {
-        return String::from("Star Trek");
+        return (String::from("STTV"), String::from("STTV"));
     } else if x.contains("StarTrek/Voyager") {
-        return String::from("Voyager");
+        return (String::from("Voyager"), String::from("Voyager"));
     } else if x.contains("StarTrek/ENT") {
-        return String::from("Enterprise");
+        return (String::from("ENT"), String::from("ENT"));
     } else if x.contains("Andor") {
-        return String::from("Andor");
+        return (String::from("Andor"), String::from("Andor"));
     } else if x.contains("BookOfBobaFett") {
-        return String::from("Boba Fett");
+        return (String::from("BobaFett"), String::from("Boba Fett"));
     } else if x.contains("Mandalorian") {
-        return String::from("Mandalorian");
+        return (String::from("Mandalorian"), String::from("Mandalorian"));
     } else if x.contains("ObiWanKenobi") {
-        return String::from("Obi Wan");
+        return (String::from("ObiWan"), String::from("Obi Wan"));
     } else if x.contains("TalesOfTheJedi") {
-        return String::from("Tales Of The Jedi");
+        return (String::from("TalesOfTheJedi"), String::from("Tales Of The Jedi"));
     } else if x.contains("TheBadBatch") {
-        return String::from("Bad Batch");
+        return (String::from("TheBadBatch"), String::from("The Bad Batch"));
     } else if x.contains("Visions") {
-        return String::from("Visions");
+        return (String::from("Visions"), String::from("Visions"));
     } else if x.contains("TheLastOfUs") {
-        return String::from("The Last Of Us");
+        return (String::from("TheLastOfUs"), String::from("The Last Of Us"));
     } else if x.contains("TheLordOfTheRingsTheRingsOfPower") {
-        return String::from("Rings Of Power");
+        return (String::from("RingsOfPower"), String::from("Rings Of Power"));
     } else if x.contains("WheelOfTime") {
-        return String::from("Wheel Of Time");
+        return (String::from("WheelOfTime"), String::from("Wheel Of Time"));
     } else if x.contains("FalconWinterSoldier") {
-        return String::from("Falcon Winter Soldier");
+        return (String::from("FalconWinterSoldier"), String::from("Falcon Winter Soldier"));
     } else if x.contains("Hawkeye") {
-        return String::from("Hawkeye");
+        return (String::from("Hawkeye"), String::from("Hawkeye"));
     } else if x.contains("IAmGroot") {
-        return String::from("I Am Groot");
+        return (String::from("IAmGroot"), String::from("I Am Groot"));
     } else if x.contains("Loki") {
-        return String::from("Loki");
+        return (String::from("Loki"), String::from("Loki"));
     } else if x.contains("MoonKnight") {
-        return String::from("Moon Knight");
+        return (String::from("MoonKnight"), String::from("Moon Knight"));
     } else if x.contains("MSMarvel") {
-        return String::from("Ms Marvel");
+        return (String::from("MSMarvel"), String::from("MS Marvel"));
     } else if x.contains("SecretInvasion") {
-        return String::from("Secret Invasion");
+        return (String::from("SecretInvasion"), String::from("Secret Invasion"));
     } else if x.contains("SheHulk") {
-        return String::from("She Hulk");
+        return (String::from("SheHulk"), String::from("She Hulk"));
     } else if x.contains("WandaVision") {
-        return String::from("WandaVision");
+        return (String::from("WandaVision"), String::from("Wanda Vision"));
     } else {
-        return String::from("Fuck");
+        return (String::from("None"), String::from("None"));
     }
     
 }
 
 pub fn process_tvshows(tv: String, count: u32) {
-    let catagory = get_tv_catagory(&tv);
     let season = get_season(&tv);
     let episode = get_episode(&tv);
     let filesize = crate::setup::mtv_utils::get_file_size(&tv);
-    let fname = get_tv_name(&tv);
+    let namef = get_tv_catagory_name(&tv);
+    let fcat = namef.0;
+    let fname = namef.1;
     let tvshow = mtv_types::TVShow {
         id: count,
         tvid: crate::setup::mtv_utils::create_md5(&tv),
         size: filesize.to_string(),
-        catagory: catagory,
+        catagory: fcat,
         name: fname,
         season: season,
         episode: episode,
