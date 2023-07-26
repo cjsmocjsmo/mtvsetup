7fcd6a811b3bd13e0d8a0188dbfc3849c4c73b78
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-07-26 13:10:24 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
+MTVP="/usr/share/mtvsetup"
+MTVSTP=$MTVP"/mtvsetup"
 
-MTVSTP="/usr/share/mtvsetup/mtvsetup"
+if [ -f /etc/systemd/system/mtvsetup.service ]; then
+    sudo systemctl stop mtvsetup.service;
+fi
 
-sudo systemctl stop mtvsetup.service;
+if [ ! -d $MTVP ]; then
+    sudo mkdir $MTVP;
+    sudo chmod +xwr $MTVP;
+    cd $MTVP;
+    git pull;
+else
+    cd $MTVP;
+    git pull;
+fi
 
-git pull;
+if [ -d $MTVSTP/thumbnails ]; then
+    sudo rm -rf $MTVSTP/thumbnails;
+    mkdir $MTVSTP/thumbnails;
+else
+    mkdir $MTVSTP/thumbnails;
+fi
 
-sudo rm -rf $MTVSTP/thumbnails;
-mkdir $MTVSTP/thumbnails;
+if [ -f $MTVSTP/mtv.db ]; then
+    sudo rm -rf $MTVSTP/mtv.db;
+    touch $MTVSTP/mtv.db;
+else
+    touch $MTVSTP/mtv.db;
+fi
 
-rm -rf $MTVSTP/mtv.db;
-touch $MTVSTP/mtv.db;
+cd $MTVSTP;
 
 cargo build --release --bin mtvsetup;
 
@@ -18,4 +38,26 @@ sudo chmod +xr $MTVSTP/target/release/mtvsetup;
 sudo chown root:root $MTVSTP/target/release/mtvsetup;
 sudo mv $MTVSTP/target/release/mtvsetup /usr/bin/mtvsetup;
 
+if [ ! -f /etc/systemd/system/mtvsetup.service ]; then
+    sudo cp -pvr $MTVSTP/mtvsetup.service /etc/systemd/system/mtvsetup.service;
+    sudo systemctl daemon-reload;
+fi
+
 sudo systemctl start mtvsetup.service;
+sudo systemctl enable mtvsetup.service;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sudo systemctl start mtvsetup.service;
+sudo systemctl status mtvsetup.service
