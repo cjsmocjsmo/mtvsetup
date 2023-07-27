89dbf98a6cf286f9b3065cd71b8cc73b0fa02b9a
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-07-27 13:58:13 -0700

    fix address

--- src/envvars.rs
+++ src/envvars.rs
@@ -32,7 +32,7 @@ pub fn set_env_vars() {
     }
     let server_addr = env::var("MTV_SERVER_ADDR");
     if server_addr.is_err() {
-        env::set_var("MTV_SERVER_ADDR", "http://http://192.168.0.94");
+        env::set_var("MTV_SERVER_ADDR", "http://192.168.0.94");
     }
     let server_port = env::var("MTV_SERVER_PORT");
     if server_port.is_err() {
