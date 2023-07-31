41523f39f02acb27d972a06dd31975d0ce696934
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-07-30 21:04:47 -0700

    update

--- src/serversetup.rs
+++ src/serversetup.rs
@@ -17,7 +17,13 @@ pub async fn run_setup_check(path: web::Path<String>) -> impl Responder {
 #[get("/setup/{status}")]
 pub async fn run_setup(path: web::Path<String>) -> impl Responder {
     let _fuck = path.into_inner();
-    let stp = setup::setup();
-    let setup = stp.to_string();
-    HttpResponse::Ok().body(setup)
+    let setup = setup::setup().to_string();
+    let file_exists = setup::mtv_tables::db_file_exists().to_string();
+    let dir_exists = setup::mtv_image::thumbnail_dir_exists().to_string();
+    let mtv_file = setup::mtv_utils::mtvsetup_file_check().to_string();
+    let mut resp = "false".to_string();
+    if setup == "true" && dir_exists == "true" && file_exists == "true" && mtv_file == "true"{
+        resp = "true".to_string();
+    };
+    HttpResponse::Ok().body(resp)
 }
