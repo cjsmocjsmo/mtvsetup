d806d84e0027b830aaa806138cbbf75c72ff8016
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-12-12 03:21:05 -0800

    Update version numbers and add debug prints

--- src/servertvs.rs
+++ src/servertvs.rs
@@ -357,8 +357,11 @@ pub async fn thecontinental(path: web::Path<String>) -> impl Responder {
 #[get("/monarchlegacyofmonsters/{season}")]
 pub async fn monarchlegacyofmonsters(path: web::Path<String>) -> impl Responder {
     let catagory = String::from("MonarchLegacyOfMonsters");
+    println!("catagory: {}", catagory);
     let season = path.into_inner();
+    println!("season: {}", season);
     let result = get_shows(catagory, season).await;
+    print!("result: {}", result);
     HttpResponse::Ok().body(result)
 }
 
