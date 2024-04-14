8ee3c4a138858930e3413e48fdebf2493215a738
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-04-14 05:21:24 -0700

    Add debug print statements to get_season function

--- src/mtv_process_tvshows.rs
+++ src/mtv_process_tvshows.rs
@@ -15,8 +15,8 @@ fn get_season(astring: &String) -> String {
     let mut seavec = Vec::new();
     let parts = season.split("S");
     for p in parts {
-        // println!("astring: {:?}", astring);
-        // println!("p: {:?}", p);
+        println!("astring: {:?}", astring);
+        println!("p: {:?}", p);
         seavec.push(p);
     }
 
