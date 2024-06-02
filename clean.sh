97751921d40862f03be8b601e0fb5e31bafc06b5
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-06-02 02:14:00 -0700

    chore: Clean up clean.sh script and update MTV database path

--- clean.sh
+++ clean.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-MTVP="/usr/share/mtvsetup/mtvsetup"
-echo "stoping mtvsetup.service";
-if [ -f /etc/systemd/system/mtvsetup.service ]; then
-    sudo systemctl stop mtvsetup.service;
-fi
-cd $MTVP;
-sudo rm -rf $MTVP/thumbnails;
-sudo rm -rf $MTVP/db;
-mkdir $MTVP/db;
-touch $MTVP/db/mtv.db;
-sudo rm -rf $MTVP/setup.mtv;
-git pull;
+# MTVP="/usr/share/mtvsetup/mtvsetup"
+# echo "stoping mtvsetup.service";
+# if [ -f /etc/systemd/system/mtvsetup.service ]; then
+#     sudo systemctl stop mtvsetup.service;
+# fi
+# cd $MTVP;
+# sudo rm -rf $MTVP/thumbnails;
+# sudo rm -rf $MTVP/db;
+# mkdir $MTVP/db;
+# touch $MTVP/db/mtv.db;
+# sudo rm -rf $MTVP/setup.mtv;
+# git pull;
 
+rm /usr/share/mtvdb/mtv.db;
+touch /usr/share/mtvdb/mtv.db;
